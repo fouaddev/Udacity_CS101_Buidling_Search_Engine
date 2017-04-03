3b42f767b89f13853cf8ccad22f67df48230ba79
Fouad Developer <fouad.b.business@gmail.com>
2017-04-03 17:45:49 -0400

    Update Problem_Set.py

--- Lesson_2/Problem_Set.py
+++ Lesson_2/Problem_Set.py
@@ -1,4 +1,268 @@
+# Define a procedure, udacify, that takes as
+# input a string, and returns a string that
+# is an uppercase 'U' followed by the input string.
+# for example, when you enter
 
+# print udacify('dacians')
+
+# the output should be the string 'Udacians'
+def udacify(s):
+    return 'U' + s
+
+# Remove the hash, #, from infront of print to test your code.
+
+print udacify('dacians')
+#>>> Udacians
+
+print udacify('turn')
+#>>> Uturn
+
+print udacify('boat')
+#>>> Uboat
+
+
+-----------------------------------------------------
+
+
+# Define a procedure, median, that takes three
+# numbers as its inputs, and returns the median
+# of the three numbers.
+
+# Make sure your procedure has a return statement.
+
+def median(x,y,z):
+    def biggest(x,y,z):
+        def bigger(x,y):
+            if x >= y:
+                return x
+            return y
+        return bigger(bigger(x,y),z)
+    if biggest(x,y,z) == x:
+        if y >= z:
+            return y
+        return z
+    elif biggest(x,y,z) == y:
+        if x >= z:
+            return x
+        return z
+    else:
+        if x >= y:
+            return x
+        return y
+
+print(median(1,2,3))
+#>>> 2
+
+print(median(9,3,6))
+#>>> 6
+
+print(median(7,8,7))
+#>>> 7
+
+
+-----------------------------------------------------
+
+
+# Define a procedure, countdown, that takes a
+# positive whole number as its input, and prints
+# out a countdown from that number to 1,
+# followed by Blastoff!
+# The procedure should not return anything.
+# For this question, you just need to call 
+# the procedure using the line
+# countdown(3)
+# instead of print countdown(3).
+def countdown(n):
+    while n > 0:
+        print n
+        n -= 1
+    print 'Blasoff!'
+countdown(3)
+#>>> 3
+#>>> 2
+#>>> 1
+#>>> Blastoff!
+
+
+-----------------------------------------------------
+
+
+# Define a procedure, find_last, that takes as input
+# two strings, a search string and a target string,
+# and returns the last position in the search string
+# where the target string appears, or -1 if there
+# are no occurrences.
+#
+# Example: find_last('aaaa', 'a') returns 3
+
+# Make sure your procedure has a return statement.
+
+def find_last(s,t):
+    pos = 0
+    while pos <= len(s) + 1:
+        last_pos = s.find(t, pos)
+        if last_pos == -1:
+            return pos - 1
+        pos += 1
+    
+print find_last('aaaa', 'a')
+#>>> 3
+
+print find_last('aaaaa', 'aa')
+#>>> 3
+
+print find_last('aaaa', 'b')
+#>>> -1
+
+print find_last("111111111", "1")
+#>>> 8
+
+print find_last("222222222", "")
+#>>> 9
+
+print find_last("", "3")
+#>>> -1
+
+print find_last("", "")
+#>>> 0
+
+
+-----------------------------------------------------
+
+
+# Define a procedure weekend which takes a string as its input, and
+# returns the boolean True if it's 'Saturday' or 'Sunday' and False otherwise.
+
+def weekend(day):
+    # your code here
+    if day == 'Saturday' or day == 'Sunday':
+        return True
+    return False
+print weekend('Monday')
+#>>> False
+
+print weekend('Saturday')
+#>>> True
+
+print weekend('July')
+#>>> False
+
+
+-----------------------------------------------------
+
+
+# Define a procedure, stamps, which takes as its input a positive integer in
+# pence and returns the number of 5p, 2p and 1p stamps (p is pence) required 
+# to make up that value. The return value should be a tuple of three numbers 
+# (that is, your return statement should be followed by the number of 5p,
+# the number of 2p, and the nuber of 1p stamps).
+#
+# Your answer should use as few total stamps as possible by first using as 
+# many 5p stamps as possible, then 2 pence stamps and finally 1p stamps as 
+# needed to make up the total.
+#
+# (No fair for USians to just say use a "Forever" stamp and be done with it!)
+#
+
+def quotient_and_reminder(divident,devisor):
+    quotient = divident / devisor
+    reminder = divident - (quotient * devisor)
+    return quotient, reminder
+
+def stamps(n):
+    p5, reminder5 = quotient_and_reminder(n,5)
+    p2, p1 = quotient_and_reminder(reminder5,2)
+    return p5, p2, p1
+    
+print stamps(8)
+#>>> (1, 1, 1)  # one 5p stamp, one 2p stamp and one 1p stamp
+print stamps(5)
+#>>> (1, 0, 0)  # one 5p stamp, no 2p stamps and no 1p stamps
+print stamps(29)
+#>>> (5, 2, 0)  # five 5p stamps, two 2p stamps and no 1p stamps
+print stamps(0)
+#>>> (0, 0, 0) # no 5p stamps, no 2p stamps and no 1p stamps
+
+
+-----------------------------------------------------
+
+
+# The range of a set of values is the maximum value minus the minimum
+# value. Define a procedure, set_range, which returns the range of three input
+# values.
+
+# Hint: the procedure, biggest which you coded in this unit
+# might help you with this question. You might also like to find a way to
+# code it using some built-in functions.
+
+def set_range(x,y,z):
+    # Your code here
+    def biggest(x,y,z):
+        def bigger(x,y):
+            if x >= y:
+                return x
+            return y
+        return bigger(bigger(x,y),z)
+    def smallest(x,y,z):
+        return -biggest(-x,-y,-z)
+    return biggest(x,y,z) - smallest(x,y,z)
+    
+print set_range(10, 4, 7)
+#>>> 6  # since 10 - 4 = 6
+
+print set_range(1.1, 7.4, 18.7)
+#>>> 17.6 # since 18.7 - 1.1 = 17.6
+
+
+-----------------------------------------------------
+
+
+# By Sam the Great from forums
+# That freaking superhero has been frequenting Udacity
+# as his favorite boss battle fight stage. The 'Udacity'
+# banner keeps breaking, and money is being wasted on
+# repairs. This time, we need you to proceduralize the
+# fixing process by building a machine to automatically
+# search through debris and return the 'Udacity' banner
+# to the company, and be able to similarly fix other goods.
+
+# Write a Python procedure fix_machine to take 2 string inputs
+# and returns the 2nd input string as the output if all of its
+# characters can be found in the 1st input string and "Give me
+# something that's not useless next time." if it's impossible.
+# Letters that are present in the 1st input string may be used
+# as many times as necessary to create the 2nd string (you
+# don't need to keep track of repeat usage).
+
+# NOTE: # If you are experiencing difficulties taking
+        # this problem seriously, please refer back to
+        # "Superhero flyby", the prequel, in Problem Set 11.
+
+# TOOLS: # if statement
+         # while loop
+         # string operations
+         # Unit 1 Basics
+
+# BONUS: # 
+# 5***** #  If you've graduated from CS101,
+#  Gold  #  try solving this in one line.
+# Stars! #
+
+def fix_machine(debris, product):
+    i = 0
+    while debris.find(product[i]):
+        return product
+        i += 1
+    return "Give me something that's not useless next time."
+    
+### TEST CASES ###
+print "Test case 1: ", fix_machine('UdaciousUdacitee', 'Udacity') == "Give me something that's not useless next time."
+print "Test case 2: ", fix_machine('buy me dat Unicorn', 'Udacity') == 'Udacity'
+print "Test case 3: ", fix_machine('AEIOU and sometimes y... c', 'Udacity') == 'Udacity'
+print "Test case 4: ", fix_machine('wsx0-=mttrhix', 't-shirt') == 't-shirt'
+
+
+-----------------------------------------------------
 
 
 # Define a procedure, find_last, that takes as input
