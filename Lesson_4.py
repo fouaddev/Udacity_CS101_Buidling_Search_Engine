e034e658b738f59cc927a3ee1c38e9c45ccaf05f
Fouad Developer <fouad.b.business@gmail.com>
2017-07-26 22:12:28 -0400

    Update and rename Lesson_4 to Lesson_4.py

--- Lesson_4.py
+++ Lesson_4.py
@@ -38,3 +38,29 @@ print index
 
 # LOOKUP
 
+# Define a procedure, lookup,
+# that takes two inputs:
+
+# - an index
+# - keyword
+
+# The procedure should return a list
+# of the urls associated
+# with the keyword. If the keyword
+# is not in the index, the procedure
+# should return an empty list.
+
+index = [['udacity', ['http://udacity.com', 'http://npr.org']],
+         ['computing', ['http://acm.org']]]
+
+def lookup(index,keyword):
+    for e in index:
+        if keyword == e[0]:
+            return e[1]
+    return []
+
+print lookup(index,'udacity')
+#>>> ['http://udacity.com','http://npr.org']
+
+####################################################################
+
