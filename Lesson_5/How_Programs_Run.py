20469a8b305cecd0fa63dc3163b337a9598ca814
Fouad Developer <fouad.b.business@gmail.com>
2018-02-28 22:07:00 -0500

    Update How_Programs_Run.py

--- Lesson_5/How_Programs_Run.py
+++ Lesson_5/How_Programs_Run.py
@@ -130,4 +130,149 @@ print table
 
 #-------------------------------------------------------------------------------
 
-# 
+# # Define a procedure,
+
+# hashtable_lookup(htable,key)
+
+# that takes two inputs, a hashtable
+# and a key (string),
+# and returns the value associated
+# with that key.
+
+def hashtable_lookup(htable, key):
+    key_bucket = hashtable_get_bucket(htable, key)
+    for e in key_bucket:
+        if e[0] == key:
+            return e[1]
+    return None
+
+def hashtable_add(htable,key,value):
+    bucket = hashtable_get_bucket(htable,key)
+    bucket.append([key,value])
+
+
+def hashtable_get_bucket(htable,keyword):
+    return htable[hash_string(keyword,len(htable))]
+
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        out = (out + ord(s)) % buckets
+    return out
+
+def make_hashtable(nbuckets):
+    table = []
+    for unused in range(0,nbuckets):
+        table.append([])
+    return table
+
+
+table = [[['Ellis', 11], ['Francis', 13]], [], [['Bill', 17], ['Zoe', 14]],
+[['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
+
+print hashtable_lookup(table, 'Francis')
+#>>> 13
+
+print hashtable_lookup(table, 'Louis')
+#>>> 29
+
+print hashtable_lookup(table, 'Zoe')
+#>>> 14
+
+#-----------------------------------------------------------------------------
+
+# UPDATE
+
+# Define a procedure, hashtable_update(htable,key,value)
+# that updates the value associated with key. If key is already in the
+# table, change the value to the new value. Otherwise, add a new entry
+# for the key and value.
+
+# Hint: Use hashtable_lookup as a starting point.
+# Make sure that you return the new htable
+
+def hashtable_update(htable,key,value):
+    bucket_target = hashtable_get_bucket(htable,key)
+    for e in bucket_target:
+        if key == e[0]:
+            e[1] = value
+    htable.append([key,value])
+    return htable
+
+def hashtable_lookup(htable,key):
+    bucket = hashtable_get_bucket(htable,key)
+    for entry in bucket:
+        if entry[0] == key:
+            return entry[1]
+    return None
+
+def hashtable_add(htable,key,value):
+    bucket = hashtable_get_bucket(htable,key)
+    bucket.append([key,value])
+
+
+def hashtable_get_bucket(htable,keyword):
+    return htable[hash_string(keyword,len(htable))]
+
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        out = (out + ord(s)) % buckets
+    return out
+
+def make_hashtable(nbuckets):
+    table = []
+    for unused in range(0,nbuckets):
+        table.append([])
+    return table
+
+
+table = [[['Ellis', 11], ['Francis', 13]], [], [['Bill', 17], ['Zoe', 14]],
+[['Coach', 4]], [['Louis', 29], ['Nick', 2], ['Rochelle', 4]]]
+
+hashtable_update(table, 'Bill', 42)
+hashtable_update(table, 'Rochelle', 94)
+hashtable_update(table, 'Zed', 68)
+print table
+#>>> [[['Ellis', 11], ['Francis', 13]], [['Zed', 68]], [['Bill', 42], 
+#>>> ['Zoe', 14]], [['Coach', 4]], [['Louis', 29], ['Nick', 2], 
+#>>> ['Rochelle', 94]]]
+
+#----------------------------------------------------------------------------
+
+# POPULATION
+
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions.
+
+population = {}
+population['Algiers'] = 2.8
+population['Shanghai'] = 17.8
+population['Istanbul'] = 13.3
+population['Karachi'] = 13.0
+population['Mumbai'] = 12.5
+print population
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+
+#-------------------------------------------------------------------------
+
+# CHANGING LOOKUP
+
+# Change the lookup procedure
+# to now work with dictionaries.
+
+def lookup(index, keyword):
+    if keyword in index:
+        return index[keyword]
+    else:
+        return None
