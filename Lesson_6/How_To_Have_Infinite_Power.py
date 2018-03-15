a79c109f28edd4289f9e37d81d432ec9b0f83beb
Fouad Developer <fouad.b.business@gmail.com>
2018-03-14 20:49:49 -0400

    Update to the page rank algorithm and the to the whole search engine script

--- Lesson_6/How_To_Have_Infinite_Power.py
+++ Lesson_6/How_To_Have_Infinite_Power.py
@@ -309,177 +309,55 @@ print graph
 #Finishing the page ranking algorithm.
 
 def compute_ranks(graph):
-    d = 0.8 # damping factor
+    d = 0.8  # damping factor
     numloops = 10
-    
+
     ranks = {}
     npages = len(graph)
     for page in graph:
         ranks[page] = 1.0 / npages
-    
+
     for i in range(0, numloops):
         newranks = {}
         for page in graph:
             newrank = (1 - d) / npages
-            
+
             for node in graph:
                 if page in graph[node]:
                     newrank = newrank + d * (ranks[node] / len(graph[node]))
-            
+
             newranks[page] = newrank
         ranks = newranks
     return ranks
 
-
-
-cache = {
-   'http://udacity.com/cs101x/urank/index.html': """<html>
-<body>
-<h1>Dave's Cooking Algorithms</h1>
-<p>
-Here are my favorite recipies:
-<ul>
-<li> <a href="http://udacity.com/cs101x/urank/hummus.html">Hummus Recipe</a>
-<li> <a href="http://udacity.com/cs101x/urank/arsenic.html">World's Best Hummus</a>
-<li> <a href="http://udacity.com/cs101x/urank/kathleen.html">Kathleen's Hummus Recipe</a>
-</ul>
-
-For more expert opinions, check out the 
-<a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a> 
-and <a href="http://udacity.com/cs101x/urank/zinc.html">Zinc Chef</a>.
-</body>
-</html>
-
-
-
-
-
-
-""", 
-   'http://udacity.com/cs101x/urank/zinc.html': """<html>
-<body>
-<h1>The Zinc Chef</h1>
-<p>
-I learned everything I know from 
-<a href="http://udacity.com/cs101x/urank/nickel.html">the Nickel Chef</a>.
-</p>
-<p>
-For great hummus, try 
-<a href="http://udacity.com/cs101x/urank/arsenic.html">this recipe</a>.
-
-</body>
-</html>
-
-
-
-
-
-
-""", 
-   'http://udacity.com/cs101x/urank/nickel.html': """<html>
-<body>
-<h1>The Nickel Chef</h1>
-<p>
-This is the
-<a href="http://udacity.com/cs101x/urank/kathleen.html">
-best Hummus recipe!
-</a>
-
-</body>
-</html>
-
-
-
-
-
-
-""", 
-   'http://udacity.com/cs101x/urank/kathleen.html': """<html>
-<body>
-<h1>
-Kathleen's Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Open a can of garbonzo beans.
-<li> Crush them in a blender.
-<li> Add 3 tablesppons of tahini sauce.
-<li> Squeeze in one lemon.
-<li> Add salt, pepper, and buttercream frosting to taste.
-</ol>
-
-</body>
-</html>
-
-""", 
-   'http://udacity.com/cs101x/urank/arsenic.html': """<html>
-<body>
-<h1>
-The Arsenic Chef's World Famous Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Kidnap the <a href="http://udacity.com/cs101x/urank/nickel.html">Nickel Chef</a>.
-<li> Force her to make hummus for you.
-</ol>
-
-</body>
-</html>
-
-""", 
-   'http://udacity.com/cs101x/urank/hummus.html': """<html>
-<body>
-<h1>
-Hummus Recipe
-</h1>
-<p>
-
-<ol>
-<li> Go to the store and buy a container of hummus.
-<li> Open it.
-</ol>
-
-</body>
-</html>
-
-
-
-
-""", 
-}
-
-def crawl_web(seed): # returns index, graph of inlinks
+def crawl_web(seed):  # returns index, graph of inlinks
     tocrawl = [seed]
     crawled = []
     graph = {}  # <url>, [list of pages it links to]
-    index = {} 
-    while tocrawl: 
+    index = {}
+    while tocrawl:
         page = tocrawl.pop()
         if page not in crawled:
             content = get_page(page)
             add_page_to_index(index, page, content)
             outlinks = get_all_links(content)
-            
-            
+
             graph[page] = outlinks
-            
-            
+
             union(tocrawl, outlinks)
             crawled.append(page)
     return index, graph
 
-
 def get_page(url):
-    if url in cache:
-        return cache[url]
-    else:
-        return None
-    
+    try:
+        import urllib
+        return urllib.urlopen(url).read()
+    except:
+        return ""
+
 def get_next_target(page):
     start_link = page.find('<a href=')
-    if start_link == -1: 
+    if start_link == -1:
         return None, 0
     start_quote = page.find('"', start_link)
     end_quote = page.find('"', start_quote + 1)
@@ -497,7 +375,6 @@ def get_all_links(page):
             break
     return links
 
-
 def union(a, b):
     for e in b:
         if e not in a:
@@ -507,7 +384,15 @@ def add_page_to_index(index, url, content):
     words = content.split()
     for word in words:
         add_to_index(index, word, url)
-        
+    return words
+
+def record_user_click(index,keyword,url):
+    urls_list_result = lookup(index,keyword)
+    if urls_list_result:
+        for entry in urls_list_result:
+            if url == entry[0]:
+                entry[1] += 1
+
 def add_to_index(index, keyword, url):
     if keyword in index:
         index[keyword].append(url)
@@ -520,13 +405,31 @@ def lookup(index, keyword):
     else:
         return None
 
-index, graph = crawl_web('http://udacity.com/cs101x/urank/index.html')
+print 'Note: Before you enter your URL address, please make sure to enter a URL that does not lead to unlimited back links'
+print 'which will never finish and return the results.'
+print 'For testing purposes, use this URL address "https://udacity.github.io/cs101x/urank/"'
+print 'It links to limited amount of Web Pages, therefor the scraping process finishes and returns with results.'
+link = str(input('Enter your URL between double quotes here: '))
+seed = link
+index, graph = crawl_web('https://udacity.github.io/cs101x/urank/')
 ranks = compute_ranks(graph)
-print ranks
 
-#>>> {'http://udacity.com/cs101x/urank/kathleen.html': 0.11661866666666663,
-#'http://udacity.com/cs101x/urank/zinc.html': 0.038666666666666655,
-#'http://udacity.com/cs101x/urank/hummus.html': 0.038666666666666655,
-#'http://udacity.com/cs101x/urank/arsenic.html': 0.054133333333333325,
-#'http://udacity.com/cs101x/urank/index.html': 0.033333333333333326,
-#'http://udacity.com/cs101x/urank/nickel.html': 0.09743999999999997}
+print ' '
+
+print 'Here is all the data scrapped as an Index list that contains all content words and Web Pages found.'
+print 'Each word is grouped with all links that contain it:'
+
+print crawl_web('https://udacity.github.io/cs101x/urank/')
+
+print ' '
+
+print 'Note: Before you enter a keyword, please make sure to enter a simple keyword like "the" or "buy" or "more"...etc,'
+print 'due to limited data scraped.'
+key = str(input('Enter your keyword between quotes here: '))
+print 'Here is a list of all the scraped links that contain the keyword "' + key + '":'
+print lookup(index,key)
+
+print ' '
+
+print 'Here is a list of all links scrapped with their ranking indicator, based on their popularity:'
+print ranks
